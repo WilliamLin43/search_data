3e4d052bae1d5fc9c6b56b78e4a80aa7fc5595a5
William Lin <69445149+WilliamLin43@users.noreply.github.com>
2021-11-23 19:33:29 +0800

    Add files via upload

--- homework3-21-1/sentences.py
+++ homework3-21-1/sentences.py
@@ -5,13 +5,13 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.text import Text
 
 
-f = open('./dataset/COVID-19_10000.txt', 'r',encoding="utf-8")
+f = open('./dataset/dailymail.txt', 'r',encoding="utf-8")
 read_words = f.read()
     
 sentences = sent_tokenize(read_words)
 
-filename="./dataset/COVID-19_10000_sentence.txt"
-fp=open(filename, "a")
+filename="./dataset/dailymail_sentence.txt"
+fp=open(filename, "a",encoding="utf-8")
 
 for sentence in sentences:
     print(sentence)
--- homework3-21-1/train-cbow.py
+++ homework3-21-1/train-cbow.py
@@ -1,25 +1,114 @@
 # imports needed and set up logging
 import gensim
 import logging
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.text import Text
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-input_file="../../dataset/dysautonomia_10000_sentence.txt"
+#input_file="../../dataset/dysautonomia_10000_sentence.txt"
+input_file="../../dataset/dailymail_sentence.txt"
 
 with open (input_file, 'rb') as f:
     for i,line in enumerate (f):
-        print(line)
+        #print(line)
         break
 
 def read_input(input_file):
 
     logging.info("reading file {0}...this may take a while".format(input_file))
 
+
+                
+    ps = PorterStemmer()
+
     with open (input_file, 'rb') as f:
         for i, line in enumerate (f):
+           
+            #全部轉成小寫
+            line = line.lower()
+        
+            #去除特殊符號  b' 
+            line = str(line).replace("b'",' ')
+            line = str(line).replace("\r'",' ')
+            line = str(line).replace("\n",' ')
+            line = str(line).replace('/',' ')
+            line = str(line).replace('.',' ')
+            line = str(line).replace('\n','') 
+            line = str(line).replace('/e/',' ')
+            line = str(line).replace('(d)',' ')
+            line = str(line).replace('s)',' ')
+            line = str(line).replace('?]',' ')
+            line = str(line).replace('?]',' ')
+            line = str(line).replace('=-B',' ')
+            line = str(line).replace('-C',' ')
+            
             #print(i)
             #print(line)
             
+            result = word_tokenize(line)
+            words = 0
+            
+            
+            for i in range(len(result)):            
+                if len(result[i]) > 0 and result[i] != '-' and result[i] != '–' and result[i] != ',':
+                    words += 1
+                    
+                    #確認是否為數字千分號
+                    pos=result[i][:-1].find(',')
+                    #print(pos)            
+                    if int(pos) > 0 and result[i][pos-1:pos].isdigit() != True :
+                        words += 1
+                    #print(res[i])
+                #去除特殊符號字元單一數字
+                if result[i] in ['-','－','?','(',')','%','[',']','+','``','"','--','*','&','.','..','#','@','`','~','{','}','$','@','!']:
+                    result[i] = ''
+                #去除特殊符號字元
+                result[i] = result[i].replace('~','')
+                result[i] = result[i].replace(',','')
+                result[i] = result[i].replace('!','')
+                result[i] = result[i].replace('@','')
+                result[i] = result[i].replace('#','')
+                result[i] = result[i].replace('$','')
+                result[i] = result[i].replace('%','')
+                result[i] = result[i].replace('^','')
+                result[i] = result[i].replace('&','')
+                result[i] = result[i].replace('*','')
+                result[i] = result[i].replace('(','')
+                result[i] = result[i].replace(')','')
+                result[i] = result[i].replace('!','')
+                result[i] = result[i].replace(':','')
+                result[i] = result[i].replace('+','')
+                result[i] = result[i].replace('=','')
+                result[i] = result[i].replace('=-','')
+                result[i] = result[i].replace(',','')
+                result[i] = result[i].replace("'",'')
+                result[i] = result[i].replace("'s",'')
+                result[i] = result[i].replace('\n','')
+                result[i] = result[i].replace(' ','')
+                
+                if len(result[i]) > 50:
+                    result[i] = ''
+                    
+                if len(result[i]) < 2:
+                    #print(result[i])
+                    result[i] = ''    
+                
+                #加入Porter's algorithm
+                result[i] = ps.stem(result[i])
+                #print(result[i])
+                
+                #去除Stopwords
+                stop_words = set(stopwords.words('english'))
+                if result[i] in stop_words:
+                    #print(result[i])
+                    result[i] = ''
+            
+            line = ''
+            for k in range(len(result)):
+                line = line +' '+result[k]
 
             if (i % 10000 == 0):
                 logging.info ("Read {0} articles".format (i))
@@ -30,9 +119,12 @@ def read_input(input_file):
 # each review item becomes a serries of words
 # so this becomes a list of lists
 documents = list (read_input (input_file))
+print(documents)
+
 logging.info ("Done reading input file")
 
-model = gensim.models.Word2Vec (documents, window=10, min_count=2, negative=5, workers=10)
+model = gensim.models.Word2Vec (documents, window=10, min_count=2, negative=5, workers=10, hs=1)
 
-model.train(documents,total_examples=len(documents),epochs=20)
-model.save("dysautonomia_word2vec-cbow-trained.model")
+model.train(documents,total_examples=len(documents),epochs=50)
+#model.save("dysautonomia_word2vec-cbow-trained.model")
+model.save("dailymail_word2vec-cbow-trained.model")
